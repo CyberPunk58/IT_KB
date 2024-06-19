e8227d88b7bf386ddb17bdeff8cc81eee4170ea9
My Name <pavelrip@mail.ru>
2024-06-19 16:20:51 +0300

    continiue learning

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Home1 page')
+    return render(request, 'main/index.html')
 
 def about(request):
     return HttpResponse('Home page')
