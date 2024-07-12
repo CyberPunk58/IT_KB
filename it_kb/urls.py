979d797c5dad057986cbec1f66ccaa030cf244a6
CyberPunk58 <pavelrip@mail.ru>
2024-07-12 16:25:42 +0300

    upper menu added

--- it_kb/urls.py
+++ it_kb/urls.py
@@ -20,9 +20,10 @@ from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
     path('login/', views.login, name='login'),
-    path('about/', views.about, name='about'),
+    path('cartridges/', views.cartridges, name='cartridges'),
     path('users/', include('users.urls', namespace='users')),
+    path('post/<int:post_id>/', views.show_post, name='post'),
 
 ]
--- main/views.py
+++ main/views.py
@@ -4,6 +4,12 @@ from django.template.loader import render_to_string
 
 menu = ["Login", "Отправка картриджей"]
 
+menu = [
+    {'title': 'Login', 'url_name': 'login'},
+    {'title': 'Cartridges', 'url_name': 'cartridges'},
+
+]
+
 data_db = [
     {'id': 1, 'title': 'OpenOffice_4.1.15_Win_x86', 'content': 'Здесь будет дистрибутив офиса', 'is_published': True},
     {'id': 2, 'title': 'Chrome', 'content': 'Здесь будет дистрибутив Chrome', 'is_published': True},
@@ -25,3 +31,12 @@ def login(request):
 
 def about(request):
     return HttpResponse('Home page')
+
+def login(request, post_id):
+    return HttpResponse("Форма для логина")
+
+def cartridges (request, post_id):
+    return HttpResponse("Форма для отправки картриджей")
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
